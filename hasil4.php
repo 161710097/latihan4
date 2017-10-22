f7f3483f50e7d2b4b1bd048126dcc756abfe2b21
161710097 <161710097@smkassalaambandung.sch.id>
2017-10-22 17:43:05 +0700

    latihan4

---
+++ hasil4.php
@@ -0,0 +1,10 @@
+<<?php 
+require_once 'latihan4.php';
+
+$data = new kuda('ihaaa','200 kg','4','2');
+
+echo 'suara kuda = '.$data->nama."<br>";
+echo ' berat kuda = '.$data->namaor."<br>";
+echo 'jumlah kaki kuda = '.$data->ttl."<br>";
+echo 'jumlah mata kuda = ' .$data->kelas."<br>";
+ ?>
---
+++ latihan4.php
@@ -0,0 +1,16 @@
+<?php  
+class kuda{
+	public $suara;
+	public $berat;
+	public $kaki;
+	public $mata;
+
+public function __construct($a,$b,$c,$d)
+{
+	$this->nama = $a;
+	$this->namaor = $b;
+	$this->ttl = $c;
+	$this->kelas = $d;
+}
+}
+?>
